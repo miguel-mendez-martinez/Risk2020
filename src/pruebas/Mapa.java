7a7bbc087523bb1448690d1a07aff90b66048855
alberto <alberto@alberto-VirtualBox>
2020-10-06 12:17:09 +0200

    añadir mapa

---
+++ src/pruebas/Mapa.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pruebas;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author alberto
+ */
+public class Mapa {
+    
+    private ArrayList<Pais> paises;
+    
+    
+    
+    public ArrayList<Pais> getPaises(){
+        return paises;
+}
+    
+    
+    
+    
+    
+    
+}
