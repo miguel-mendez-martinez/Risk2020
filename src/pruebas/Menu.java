8d830674797a811cbe5983624446fef041155a22
migue <migue@DESKTOP-09ACPAJ.mshome.net>
2020-10-12 11:15:29 +0200

    he hecho un scanf

--- src/pruebas/Menu.java
+++ src/pruebas/Menu.java
@@ -8,6 +8,8 @@ package pruebas;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
@@ -20,83 +22,96 @@ public class Menu {
     // necesario acceder durante la ejecución del programa como, por ejemplo,
     // el mapa o los jugadores
     ArrayList<Jugador> jugadores;
+    String eleccion;
     /**
      * 
      */
     public Menu() {
         // Inicialización de algunos atributos
         // Iniciar juego
-        jugadores = new ArrayList<>();
-        String orden= null;
-        BufferedReader bufferLector= null;
-        try {
-            File fichero=  new File("comandos.csv");
-            FileReader lector= new FileReader(fichero);
-            bufferLector= new BufferedReader(lector);
-            while((orden= bufferLector.readLine())!=null) {
-                System.out.println("$> " + orden);
-                String[] partes=orden.split(" ");
-                String comando= partes[0];
-                // COMANDOS INICIALES PARA EMPEZAR A JUGAR
-                //    crear mapa
-                //    crear jugadores <nombre_fichero>
-                //    crear <nombre_jugador> <nombre_color>
-                //    asignar misiones
-                //    asignar paises <nombre_fichero>
-                //    asignar <nombre_pais> <nombre_jugador>
+        System.out.println("Prefiere iniciar por comandos o con fichero? c/f? ");
 
-                // COMANDOS DISPONIBLES DURANTE EL JUEGO
-                //    acabar
-                //    atacar <nombre_pais> <nombre_pais>
-                //    describir continente <nombre_continente>
-                //    describir frontera <nombre_pais>
-                //    describir frontera <nombre_continente>
-                //    describir jugador <nombre_jugador>
-                //    describir pais <nombre_pais>
-                //    jugador
-                //    repartir ejercitos
-                //    ver mapa
-                //    ver pais <nombre_pais>
-                switch(comando) {
-                    case "crear":
-                        if(partes.length==2) {
-                            if(partes[1].equals("mapa")) {
-                                // crearMapa es un método de la clase Menú desde el que se puede invocar
-                                // a otros métodos de las clases que contienen los atributos y los métodos
-                                // necesarios para realizar esa invocación 
-                                crearMapa();
+        try{
+            BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+            eleccion = bufferRead.readLine();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+        if(eleccion.equals("f")){
+            jugadores = new ArrayList<>();
+            String orden= null;
+            BufferedReader bufferLector= null;
+            try {
+                File fichero=  new File("comandos.csv");
+                FileReader lector= new FileReader(fichero);
+                bufferLector= new BufferedReader(lector);
+                while((orden= bufferLector.readLine())!=null) {
+                    System.out.println("$> " + orden);
+                    String[] partes=orden.split(" ");
+                    String comando= partes[0];
+                    // COMANDOS INICIALES PARA EMPEZAR A JUGAR
+                    //    crear mapa
+                    //    crear jugadores <nombre_fichero>
+                    //    crear <nombre_jugador> <nombre_color>
+                    //    asignar misiones
+                    //    asignar paises <nombre_fichero>
+                    //    asignar <nombre_pais> <nombre_jugador>
+
+                    // COMANDOS DISPONIBLES DURANTE EL JUEGO
+                    //    acabar
+                    //    atacar <nombre_pais> <nombre_pais>
+                    //    describir continente <nombre_continente>
+                    //    describir frontera <nombre_pais>
+                    //    describir frontera <nombre_continente>
+                    //    describir jugador <nombre_jugador>
+                    //    describir pais <nombre_pais>
+                    //    jugador
+                    //    repartir ejercitos
+                    //    ver mapa
+                    //    ver pais <nombre_pais>
+                    switch(comando) {
+                        case "crear":
+                            if(partes.length==2) {
+                                if(partes[1].equals("mapa")) {
+                                    // crearMapa es un método de la clase Menú desde el que se puede invocar
+                                    // a otros métodos de las clases que contienen los atributos y los métodos
+                                    // necesarios para realizar esa invocación 
+                                    crearMapa();
+                                } else {
+                                    System.out.println("\nComando incorrecto.");
+                                }
+                            } if(partes.length==3) {
+                                if(partes[1].equals("jugadores")) { 
+                                    crearJugador(new File(partes[2]));
+                                } else {
+                                    crearJugador(partes[1], partes[2]);
+                                }
                             } else {
                                 System.out.println("\nComando incorrecto.");
                             }
-                        } if(partes.length==3) {
-                            if(partes[1].equals("jugadores")) { 
-                                crearJugador(new File(partes[2]));
+                            break;
+                        case "asignar":
+                            if(partes.length!=3) {
+                                System.out.println("\nComando incorrecto.");
+                            } else if(partes[1].equals("paises")) {
+                                // asignarPaises es un método de la clase Menu que recibe como entrada el fichero
+                                // en el que se encuentra la asignación de países a jugadores. Dentro de este
+                                // método se invocará a otros métodos de las clases que contienen los atributos
+                                // y los métodos necesarios para realizar esa invocación
+                                asignarPaises(new File(partes[2]));
                             } else {
-                                crearJugador(partes[1], partes[2]);
+                                asignarPaises(partes[1], partes[2]);
                             }
-                        } else {
+                            break;
+                        default:
                             System.out.println("\nComando incorrecto.");
-                        }
-                        break;
-                    case "asignar":
-                        if(partes.length!=3) {
-                            System.out.println("\nComando incorrecto.");
-                        } else if(partes[1].equals("paises")) {
-                            // asignarPaises es un método de la clase Menu que recibe como entrada el fichero
-                            // en el que se encuentra la asignación de países a jugadores. Dentro de este
-                            // método se invocará a otros métodos de las clases que contienen los atributos
-                            // y los métodos necesarios para realizar esa invocación
-                            asignarPaises(new File(partes[2]));
-                        } else {
-                            asignarPaises(partes[1], partes[2]);
-                        }
-                        break;
-                    default:
-                        System.out.println("\nComando incorrecto.");
+                    }
                 }
+            } catch(Exception excepcion) {
+                excepcion.printStackTrace();
             }
-        } catch(Exception excepcion) {
-            excepcion.printStackTrace();
         }
     }
 
@@ -147,7 +162,10 @@ public class Menu {
                 color= partes[1];
                 crearJugador(nombre, color);
             }
-            
+            System.out.println("Array final de jugadores tras la lectura completa del archivo:");
+            for (int i=0;i<jugadores.size();i++) {
+                System.out.println(jugadores.get(i).toString());
+            }
         }catch(Exception excepcion) {
             excepcion.printStackTrace();
         }
@@ -188,6 +206,9 @@ public class Menu {
                     jugadores.add(jugador);
                 }
         }
-        System.out.println(jugadores.toString());
+        /*System.out.println("Array de jugadores despues de la insercion/error:");
+        for (int i=0;i<jugadores.size();i++) {
+            System.out.println(jugadores.get(i).toString());
+        }*/
     }
 }
